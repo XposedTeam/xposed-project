461c2a48c9d8a8433a8aaf12c20700bd9ba1230e
Jayson Galban <100999803+JeeSenpai@users.noreply.github.com>
2024-05-28 23:20:09 +0800

    Continuation on Home Page
    - Added leaderboard section
    - Added stream section
    - Added team section
    - Added some images and video stream

--- frontend/src/sections/MainSection.vue
+++ frontend/src/sections/MainSection.vue
@@ -11,10 +11,10 @@
 				</div>
 			</div>
 		</div>
-		
-
 	</section>
-	<section class="h-[1700px] page2-bg">
+
+	<!-- About Section -->
+	<section class="h-[1700px] about-bg">
 		<div class="flex gap-8 flex-col md:flex-row items-center">
             <h1 class="mt-32 ml-32">
                 <img src="../assets/dice.png" class="min-w-[600px]" alt="">
@@ -52,15 +52,104 @@
 			</div>
         </div>
 	</section>
-	<!-- <section class="h-screen">
-		qeqqe
-	</section> -->
-	<!-- Main section end -->
+
+	<!-- Leaderboard Section -->
+	<section class="h-[1300px] leaderboard-bg p-32">
+		<div class="h-[1000px] leaderboard-card rounded-xl mt-10">
+			<img src="../assets/leaderboard.png" alt="image" class="block mx-auto w-[500px] lead-img">
+			<div class="flex flex-row justify-center">
+				<div class="mt-60 text-center">
+					<img src="../assets/silvertrophy.png" alt="Image 1">
+					<p class="text-white text-3xl font-bold" style="font-family: Saira Condensed, sans-serif;">#Player202</p>
+				</div>
+				<div class="mt-36 text-center">
+					<img src="../assets/goldtrophy.png" alt="Image 2">
+					<p class="text-white text-3xl font-bold" style="font-family: Saira Condensed, sans-serif;">#Player201</p>
+				</div>
+				<div class="mt-60 text-center">
+					<img src="../assets/bronzetrophy.png"  alt="Image 3">
+					<p class="text-white text-3xl font-bold" style="font-family: Saira Condensed, sans-serif;">#Player203</p>
+				</div>
+			</div>
+			<div class="flex justify-center mt-6" style="font-family: Saira Condensed, sans-serif;">
+				<table>
+					<tr>
+						<th class="px-32 py-8 text-white text-2xl font-bold">User</th>
+						<th class="px-32 py-8 text-white text-2xl font-bold">Amount</th>
+						<th class="px-32 py-8 text-white text-2xl font-bold">Price</th>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+					<tr>
+						<td class="px-32 py-3 text-white font-semibold">ski*******</td>
+						<td class="px-32 py-3 text-white font-semibold">$ 1,706,033</td>
+						<td class="px-32 py-3 text-red-600 font-semibold">$ 15,000</td>
+					</tr>
+				</table>
+			</div>
+		</div>
+	</section>
+
+	<!-- Stream Section -->
+	<section class="h-[1000px] stream-bg">
+			<div class="flex justify-center" style="font-family: Saira Condensed, sans-serif;">
+			<div class="mt-28 text-white text-3xl font-bold uppercase">Catch every video on <a :href="channelUrl" target="_blank" rel="noopener noreferrer">@XposedLIVE</a></div>
+			</div>
+			<!-- Embed YouTube Channel -->
+			<div class="flex justify-center mt-8">
+			<iframe
+				width="1080"
+				height="650"
+				:src="embedUrl"
+				title="YouTube Channel"
+				frameborder="0"
+				allowfullscreen
+			></iframe>
+			</div>
+	</section>
+
+	<!-- Team Section -->
+	<section class="h-screen team-bg relative overflow-hidden">
+		<div class="flex justify-start">
+		<div class="text-white text-[60px] lg:text-[80px] font-bold uppercase mt-16 ml-16" style="font-family: Stick No Bills, sans-serif; line-height: 0.8;">The Exposed Team</div>
+		</div>
+		<div class="absolute inset-x-0 bottom-0 -mb-32 flex justify-center items-end opacity-20">
+		<img class="h-auto max-w-full" src="../assets/team.png" alt="The Exposed Team">
+		</div>
+	</section>
 </template>
 
 <script>
 export default {
-
+	data() {
+		return {
+			channelUrl: "https://www.youtube.com/@XposedLIVE",
+			embedUrl: "https://www.youtube.com/embed/1XizP7Y4tK0"
+		};
+	}
 }
 </script>
 
@@ -79,7 +168,7 @@ export default {
 	left:0;right:0;bottom:0;
 }
 
-.page2-bg {
+.about-bg {
 	background: linear-gradient(180deg, 
     rgba(56, 2, 209, 0.86) 10%, 
     rgba(126, 0, 0, 0.86) 40%, 
@@ -88,4 +177,27 @@ export default {
 	left:0;right:0;bottom:0;
 }
 
+.leaderboard-bg {
+	background: linear-gradient(180deg, rgba(0, 0, 0, 0.86) 40%, rgba(153, 0, 0, 0.86) 80%);
+}
+
+.leaderboard-card {
+	background: linear-gradient(180deg, rgba(0, 0, 0, 0.86) 50%, rgba(153, 0, 0, 0.86) 80%);
+	position: relative;
+}
+
+.leaderboard-card .lead-img {
+	position: absolute;
+	top: -80px;
+	left: 50.5%;
+	transform: translateX(-50%);
+}
+
+.stream-bg {
+	background: radial-gradient(50% 50% at 50% 50%, rgba(153, 1, 1, 0.86) 41.5%, rgba(43, 2, 209, 0.86) 100%);
+}
+
+.team-bg {
+	background: linear-gradient(180deg, rgba(126, 0, 0, 0.86) 0%, rgba(0, 0, 0, 0.86) 100%);
+}
 </style>
